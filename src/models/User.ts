c01277af17b4694ae28c8653d34520a8bad5e408
kumarlalait79 <luckyarya0101@gmail.com>
2026-02-27 16:56:23 +0530

    auth module start

---
+++ src/models/User.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+interface IUser {
+  name: string;
+  email: string;
+  phoneNumber: string;
+  username: string;
+  password: string;
+  booksAdded?: string[];
+  roles: string;
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+  name: { type: String, trim: true, required: true },
+  email: { type: String, trim: true, required: true },
+  phoneNumber: { type: String, trim: true, required: true },
+  username: { type: String, trim: true, required: true, unique: true },
+  password: { type: String, trim: true, required: true },
+  booksAdded: { type: mongoose.Schema.Types.ObjectId, ref: "Book" },
+  roles: { type: String, enum: ["creator", "visitor", "admin"] },
+});
+
+const User = mongoose.model<IUser>("User", userSchema);
+
+export type { IUser };
+export default User;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import express from "express"
 import bookRouter from "./book.routes.js"
+import authRouter from "./auth.routes.js"
 
 const router = express.Router()
 
 router.use("/books", bookRouter)
+
+router.use("/auth", authRouter)
 
 export default router
