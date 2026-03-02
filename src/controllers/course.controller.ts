df5af787033b04b7eb653623a644bbe1690e422b
kumarlalait79 <luckyarya0101@gmail.com>
2026-03-02 12:18:10 +0530

    cached get route

--- src/controllers/course.controller.ts
+++ src/controllers/course.controller.ts
@@ -1,8 +1,19 @@
 import type { Request, Response } from "express";
 import Course from "../models/Course.js";
+import redis from "../utils/redis.js";
 
 export const getAllCourse = async (req: Request, res: Response) => {
   try {
+    const cacheKey = "course:all";
+    const cachedData = await redis.get(cacheKey);
+    if (cachedData) {
+      return res.status(200).json({
+        message: "Courses fetched from cache",
+        course: JSON.parse(cachedData),
+        success: true,
+      });
+    }
+
     const course = await Course.find();
     if (!course) {
       return res.status(400).json({
@@ -11,8 +22,10 @@ export const getAllCourse = async (req: Request, res: Response) => {
       });
     }
 
+    await redis.set(cacheKey, JSON.stringify(course), "EX", 60)
+
     return res.status(200).json({
-      message: "All course fetched",
+      message: "All course fetched from DB",
       course,
       success: true,
     });
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,47 @@
-import express, { type Request, type Response } from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import { connectDb } from "./utils/db.js"
-import router from "./routes/index.js"
+import express, { type Request, type Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDb } from "./utils/db.js";
+import router from "./routes/index.js";
+import redis from "./utils/redis.js";
 
-const app = express()
+const app = express();
 
-dotenv.config()
-const port = process.env.PORT || 2000
+dotenv.config();
+const port = process.env.PORT || 2000;
 
-await connectDb()
+await connectDb();
 
-app.use(cors({
-    origin: process.env.HOST_URL || "*"
-}))
+app.use(
+  cors({
+    origin: process.env.HOST_URL || "*",
+  }),
+);
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/api" , router)
+app.use("/api", router);
 
-app.get("/", (req: Request, res: Response) => {
-    return res.status(200).json({
-        message: "Everything is fine!"
-    })
-})
 
-app.listen(port, () => console.log(`Server is running at port ${port}`))
+// Normal testing express server
+// app.get("/", (req: Request, res: Response) => {
+//   return res.status(200).json({
+//     message: "Everything is fine!",
+//   });
+// });
+
+// testing redis server
+app.get("/", async (req: Request, res: Response) => {
+  await redis.set("name", "Lalit");
+
+  const value = await redis.get("name");
+
+  res.json({
+    success: true,
+    value,
+  });
+});
+
+
+
+app.listen(port, () => console.log(`Server is running at port ${port}`));
